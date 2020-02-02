ca753c4c0ee0ca0f21e8e413b5687ec7d109dac1
senth542002 <senth542002@gmail.com>
2020-02-02 15:15:51 +0530

    Updates

---
+++ precache-manifest.f2af170ee120948855499022b43db062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940c21774c8714931915ed3f1dd0fa0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f795da80b5f15c985a3f",
+    "url": "/monsters-rolodex/static/css/main.234af4c7.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f795da80b5f15c985a3f",
+    "url": "/monsters-rolodex/static/js/main.318c70f7.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
